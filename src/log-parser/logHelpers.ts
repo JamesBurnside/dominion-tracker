45ec8cb032ba41eeb4ee45eb2d96b81e4b8404b5
jakob-manning <70977283+jakob-manning@users.noreply.github.com>
2020-12-19 16:38:39 -0800

    Revert "Merge branch 'main' of C:\Users\Jakob\WebstormProjects\dominion-tracker with conflicts."
    This reverts commit 82fc28bb3b2c6b9f8034202d5e79fd41fb46fcf6.

--- src/log-parser/logHelpers.ts
+++ src/log-parser/logHelpers.ts
@@ -1,50 +1,86 @@
-import { DominionAction, DominionLog, DominionLogs, DominionSubject } from "@types";
+import { DominionAction, DominionLog, DominionLogs, KnownActions } from "@types";
+import logger from "logger";
 import { extractActionFromLogLine } from "utils/actionHelper";
-import { extractSubjectsFromLogLine } from "utils/subjectHelper";
+import { extractSubjectFromLogLine } from "utils/subjectHelper";
 
-export const getLogContainer = (): HTMLElement => document.getElementById("log-container");
+const LOG_CONTAINER_CLASS_NAME = "log-container";
+const LOG_LINE_CLASS_NAME = "log-line";
 
-export const getLogsFromContainer = (logContainer: HTMLElement): DominionLogs =>
-	convertLogStringsToLogs(getLogsAsStringsFromContainer(logContainer));
+/**
+ * TODO: Check if this is the correct ID name from dominion.
+ */
+export const getLogContainer = (): HTMLElement => document.getElementsByClassName(LOG_CONTAINER_CLASS_NAME)[0] as HTMLElement;
 
+/**
+ * Extract an array of DominionLogs from the HTML log container element.
+ */
+export const getLogsFromContainer = (logContainer: HTMLElement): DominionLogs => Array
+	// extract log html elements as array of HTMLElements
+	.from(logContainer.getElementsByClassName(LOG_LINE_CLASS_NAME))
+	// convert html elements to string
+	.map(log => convertLogAsHTMLElementToString(log as HTMLElement))
+	// filter logs to remove only the logs we care about
+	.filter((log) => isValidLogString(log))
+	// convert the log strings to a usable DominionLog type
+	.map(log => convertLogStringToLog(log));
+
+/**
+ * Get the log text out of the html element.
+ * TODO: For now just use .innerText. This likely won't cover all scenarios and may need updated.
+ */
+export const convertLogAsHTMLElementToString = (logElement: HTMLElement): string => logElement.innerText;
+
+
+/**
+ * Check if the log line contains any known action - supported or unsupported.
+ * Also ignore some known log lines such as "Turn 1 - Player1"
+ * TODO: these extra lines really should be filtered out before this function is called.
+ */
+export const hasKnownAction = (logString: string): boolean =>
+	logString &&
+		(
+			logString.includes("Turn ") ||
+			logString.startsWith("Game ") ||
+			logString.startsWith("Kingdom generated with ") ||
+			logString.includes("Between Turns")||
+			KnownActions.some(action => logString.includes(action))
+		);
+
+/**
+ * Currently a valid log is simply a logline that contains a known action.
+ * This may need changed as more edge cases are discovered.
+ */
 export const isValidLogString = (logString: string): boolean => {
 	// Quickly check that the log string contains a valid action
-	const validActions = Object.values(DominionAction);
-	return logString && validActions.some(action => logString.includes(action));
+	const supportedActions = Object.values(DominionAction);
 
-	// Check if there was an unknown action and log an error
-	// !(logString.startsWith("Game ") && logString.endsWith("rated.")) &&
-	// !(logString.startsWith("Kingdom generated with")) &&
-	// !(logString.match((new VerEx()).startOfLine().digit().oneOrMore().then("%").then(":"))) &&
-	// !(logString.match((new VerEx()).startOfLine().then("Turn ").digit().then(" - ")));
+	if (!hasKnownAction(logString)) {
+		logger.error(`Log contains no know action: ${logString}`);
+	}
+
+	return logString && supportedActions.some(action => logString.includes(action));
 }
 
-export const getLogsAsStringsFromContainer = (logContainer: HTMLElement): string[] => {
-	const logs = logContainer.innerHTML;
-	return logs.split(/\r?\n/).filter((logline) => isValidLogString(logline));
-}
-
-export const convertLogStringsToLogs = (logsAsStrings: string[]): DominionLogs =>
-	logsAsStrings.map((logAsString) => convertLogStringToLog(logAsString));
-
+/**
+ * Converts a log that is a string to a usable DominionLog.
+ */
 export const convertLogStringToLog = (logAsString: string): DominionLog => {
 	// Extract player - Be trivial about this for now and assume
 	// players dont have spaces in their name.
-	const playerName = logAsString.split(" ")[0];
+	const playerName = logAsString.split(/\s/gm)[0];
+
+	//slice player name from log string
+	const logWithoutPlayerName = logAsString.slice(playerName.length).trim()
 
 	// Extract action
-	const primaryAction = extractActionFromLogLine(logAsString);
+	const action = extractActionFromLogLine(logWithoutPlayerName);
 
 	// Extract subject
-<<<<<<< Updated upstream
-	const primarySubject = extractSubjectFromLogLine(logAsString);
-=======
-	const subject = extractSubjectsFromLogLine(logWithoutPlayerName);
->>>>>>> Stashed changes
+	const subject = extractSubjectFromLogLine(logWithoutPlayerName);
 
 	return {
 		playerName,
-		primaryAction,
-		primarySubject
+		action,
+		subject
 	}
 }
--- src/utils/subjectHelper.test.ts
+++ src/utils/subjectHelper.test.ts
@@ -1,32 +1,32 @@
 import { DominionSubjectType } from "@types";
-import {extractSubjectsFromLogLine, parseQualifierToInt, isCardNameOfficial, cardDepluralizer} from "./subjectHelper";
+import {extractSubjectFromLogLine, parseQualifierToInt, isCardNameOfficial, cardDepluralizer} from "./subjectHelper";
 
 const unsupportedCard = { type: DominionSubjectType.Unsupported };
 
 describe("Subject helper tests", () => {
 	test("extractSubjectFromLogLine should return expected subjects", () => {
 		// falsey tests
-		expect(extractSubjectsFromLogLine("turtles")).toEqual(unsupportedCard);
-		expect(extractSubjectsFromLogLine("L buys and gains ")).toEqual(unsupportedCard);
-		expect(extractSubjectsFromLogLine("L gains 2 JAKOBSS")).toEqual(unsupportedCard);
+		expect(extractSubjectFromLogLine("turtles")).toEqual(unsupportedCard);
+		expect(extractSubjectFromLogLine("L buys and gains ")).toEqual(unsupportedCard);
+		expect(extractSubjectFromLogLine("L gains 2 ESSTATIES")).toEqual(unsupportedCard);
 
 		// thruthy tests
-		expect(extractSubjectsFromLogLine("L buys and gains a Silver.")).toEqual({
+		expect(extractSubjectFromLogLine("L buys and gains a Silver")).toEqual({
 			type: DominionSubjectType.Card,
-			card: "silver",
+			card: "Silver",
 			amount: 1
 		});
-		expect(extractSubjectsFromLogLine("L buys and gains a Silver.")).toEqual({
+		expect(extractSubjectFromLogLine("L buys and gains a Silver.")).toEqual({
 			type: DominionSubjectType.Card,
-			card: "silver",
+			card: "Silver",
 			amount: 1
 		});
-		expect(extractSubjectsFromLogLine("L buys and gains a Silver from trash.")).toEqual({
+		expect(extractSubjectFromLogLine("L buys and gains a Silver from trash.")).toEqual({
 			type: DominionSubjectType.Card,
-			card: "silver",
+			card: "Silver",
 			amount: 1
 		});
-		expect(extractSubjectsFromLogLine("L gains 2 Silvers from trash.")).toEqual({
+		expect(extractSubjectFromLogLine("L gains 2 Silvers from trash.")).toEqual({
 			type: DominionSubjectType.Card,
 			card: "Silver",
 			amount: 2
@@ -46,13 +46,13 @@ describe("Subject helper tests", () => {
 	//TODO: expand this test to include plural numbers
 	test("extractSubjectFromLogLine handles basic numbers", () => {
 		// gains
-		expect(extractSubjectsFromLogLine("L gains 2 Silver.")).toEqual({
+		expect(extractSubjectFromLogLine("L gains 2 Silver")).toEqual({
 			type: DominionSubjectType.Card,
 			card: "Silver",
 			amount: 2
 		});
 		//trashes
-		expect(extractSubjectsFromLogLine("L trashes 51023 Silver.")).toEqual({
+		expect(extractSubjectFromLogLine("L trashes 51023 Silver.")).toEqual({
 			type: DominionSubjectType.Card,
 			card: "Silver",
 			amount: 51023
@@ -72,6 +72,8 @@ describe("Subject helper tests", () => {
 		expect(cardDepluralizer("Copper")).toEqual("Copper");
 		//plural
 		expect(cardDepluralizer("Coppers")).toEqual("Copper");
+		//exists in the plurals map
+		expect(cardDepluralizer("Platina")).toEqual("Platinum");
 		//undefined
 		expect(cardDepluralizer("CopperssZss")).toEqual(undefined);
 	});
