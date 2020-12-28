5b86e2661308195e4d8e84a64d8130bd4eab4b50
James Burnside <mail@jamesburnside.com>
2020-12-27 16:28:53 -0800

    quickly add total

--- src/web-components/dominion-deck.ts
+++ src/web-components/dominion-deck.ts
@@ -11,12 +11,20 @@ export class DominionDeckHtmlElement extends HTMLElement {
 			this.className += "no-deck";
 			this.textContent = "no deck :(";
 		} else {
+			let total = 0;
 			for (const [card, amount] of this._deck.entries()) {
 				const cardAmountElement = document.createElement("div");
 				cardAmountElement.className = "card-amount";
 				cardAmountElement.textContent = `${card}: ${amount}`;
-				this.appendChild(cardAmountElement)
+				this.appendChild(cardAmountElement);
+
+				total += amount;
 			}
+
+			const totalElement = document.createElement("div");
+			totalElement.className = "card-amount total-amount";
+			totalElement.textContent = `Total: ${total}`;
+			this.appendChild(totalElement);
 		}
 	}
 
