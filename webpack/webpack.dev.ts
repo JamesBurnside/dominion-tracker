8ee8ac5264060bab46befb2d673a3d58317d8f40
James Burnside <mail@jamesburnside.com>
2021-02-28 15:01:50 -0800

    create dev and prod icons, update commands to just npm run dev and npm package, update npm package to create the zip to instantly upload

--- webpack/webpack.dev.ts
+++ webpack/webpack.dev.ts
@@ -1,7 +1,13 @@
 import { merge } from "webpack-merge";
 import common from "./webpack.common";
+import CopyPlugin from "copy-webpack-plugin";
 
 export default merge(common, {
 	devtool: "inline-source-map",
-	mode: "development"
+	mode: "development",
+	plugins: [
+		new CopyPlugin({
+			patterns: [{ from: ".", to: "../icons", context: "icons/icons-dev" }]
+		})
+	]
 });
